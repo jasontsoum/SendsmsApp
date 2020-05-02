e6925fca14ecee3c288abf049bdbcbf097b725df
Jasonuom1147 <mai1147@uom.edu.gr>
2020-05-02 22:01:31 +0300

    Fix Store Variable (shared preferences) at name and address

--- sendsms2/src/com/example/sendsms2/MainActivity.java
+++ sendsms2/src/com/example/sendsms2/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.Menu;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.widget.Button;
 import android.widget.EditText;
 import android.telephony.SmsMessage;
@@ -21,16 +22,54 @@ public class MainActivity extends Activity {
 	
 	EditText txtnumber, txtname, txtaddress;
 	
-	Button btnsend;
+	Button btnsend, btnsave, btnclear;
+	
+	SharedPreferences myprefs;
+	
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		txtnumber=(EditText)findViewById(R.id.editText2); //number
-		txtname =(EditText)findViewById(R.id.editText1); //name
-		txtaddress=(EditText)findViewById(R.id.editText3); //address cffc
+	//	txtname =(EditText)findViewById(R.id.editText1); //name
+	//	txtaddress=(EditText)findViewById(R.id.editText3); //address
 		btnsend=(Button)findViewById(R.id.button1);
+		btnsave=(Button)findViewById(R.id.button2);
+		btnclear=(Button)findViewById(R.id.button3);
+		
+	
+		myprefs = getPreferences(MODE_PRIVATE);
+		
+		init();
+		btnclear.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {	
+				SharedPreferences.Editor editor = myprefs.edit();
+				editor.clear();
+				editor.commit();
+				readreferences();
+				
+			}
+		});
+		
+		btnsave.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+					
+				SharedPreferences.Editor editor = myprefs.edit();
+				String nameofperson=txtname.getText().toString();
+				String addressperson=txtaddress.getText().toString();
+				editor.putString("keyname", nameofperson);
+				editor.putString("keyname2", addressperson);
+				editor.commit();
+				}
+		});
+		
+		
 		btnsend.setOnClickListener(new View.OnClickListener() {
 				
 				@Override
@@ -39,8 +78,6 @@ public class MainActivity extends Activity {
 	                String phoneNo = "13033";
 	               // String numberout = txtnumber.getText().toString(); 
 	                String name_cl = txtname.getText().toString() + txtaddress.getText().toString();  
-	                
-	                //home address of person  
 	                String address_cl=txtaddress.getText().toString();
 	                
 	                if (phoneNo.length()>0 && name_cl.length()>0 && address_cl.length()>0)                
@@ -51,6 +88,7 @@ public class MainActivity extends Activity {
 	                        Toast.LENGTH_SHORT).show();
 	            }
 			});
+		
 		
 		}
 	private void sendSMS(String phoneNumber, String message)
@@ -72,5 +110,20 @@ public class MainActivity extends Activity {
 		}
 		
        
-    }    
+    } 
+
+	public void readreferences(){	
+		String st1=myprefs.getString("keyname", "");
+		txtname.setText(st1);
+		
+		
+		String st2=myprefs.getString("keyname2", "");
+		txtaddress.setText(st2);
+	}
+	private void init(){
+		txtname =(EditText)findViewById(R.id.editText1);
+		txtaddress=(EditText)findViewById(R.id.editText3);
+		readreferences();
+	}
+	
 }
